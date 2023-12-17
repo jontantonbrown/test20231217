98c3e6ad72a60dcf87741218770cbf93603021d7
jonta <jontantonbrown@gmail.com>
2023-12-17 19:16:25 +0000

    Update 1.

---
+++ main.py
@@ -0,0 +1,13 @@
+def main(version):
+    """
+    This is some documentation.
+    :param version:
+    :return:
+    """
+    print(f'This is version {version}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    version = 1
+    main(version)
+
